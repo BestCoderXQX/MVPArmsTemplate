780c0f378c22d2774959ffe8f4be8cdd722a02b1
GitHub Action <action@github.com>
2021-12-15 07:47:46 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/bestcoderxqx/mvparmstemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.bestcoderxqx.mvparmstemplate.services
+
+import com.github.bestcoderxqx.mvparmstemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
